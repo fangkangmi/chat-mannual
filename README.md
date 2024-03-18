733e85ccfcf9c30ae04867733e3e76d3e5e33438
fangkang <fangkangmi@outlook.com>
2024-03-18 19:58:35 +0000

    Add database and ddl file

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # chat-mannual
 Sprint:
 https://www.notion.so/chat_manual-f79fd2f1542540f5b96515032bcb2d5b
+
+Create a mysql server instance:
+ docker run -itd --name <container_name> -p 3306:3306 -e MYSQL_ROOT_PASSWORD=<Yourpassword> MYSQL_DATABASE=sqldb -d
+
+Connect to mySQL container:
+jdbc:mysql://localhost:<port>/<dbname>?allowPublicKeyRetrieval=true&useSSL=false
---
+++ Sample_table_ddl.sql
@@ -0,0 +1,37 @@
+CREATE TABLE `account` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `owner` VARCHAR(255) NOT NULL,
+  `balance` BIGINT NOT NULL,
+  `currency` VARCHAR(255) NOT NULL,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `username` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `entries` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `account_id` INT NOT NULL,
+  `amount` BIGINT NOT NULL,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+);
+
+CREATE TABLE `transfers` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `from_account_id` INT NOT NULL,
+  `to_account_id` INT NOT NULL,
+  `amount` BIGINT NOT NULL,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (`from_account_id`) REFERENCES `account` (`id`),
+  FOREIGN KEY (`to_account_id`) REFERENCES `account` (`id`)
+);
+
+CREATE INDEX `account_index_0` ON `account` (`owner`);
+
+CREATE INDEX `entries_index_1` ON `entries` (`account_id`);
+
+CREATE INDEX `transfers_index_2` ON `transfers` (`from_account_id`);
+
+CREATE INDEX `transfers_index_3` ON `transfers` (`to_account_id`);
+
+CREATE INDEX `transfers_index_4` ON transfers (from_account_id, to_account_id);

61b0950cba25f4e5dd59fdcef177d98b5d3a358b
fangkang <fangkangmi@outlook.com>
2024-03-22 16:02:27 +0000

    Added CRUD

--- cmd/main.go
+++ cmd/main.go
@@ -1,44 +1,136 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
+	"os"
+	"strings"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/julienschmidt/httprouter"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	http.ServeFile(w, r, "static/index.html")
+var db *sql.DB
+
+const port = 3306
+const dbname = "sqldb"
+
+type Item struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
-func fileUploadHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	r.ParseMultipartForm(10 * 1024 * 1024) //size 10 MB
-	file, _, err := r.FormFile("file")
+func initDB() error {
+	var err error
+	user := os.Getenv("MYSQL_USER")
+	pass := os.Getenv("MYSQL_PASS")
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(localhost:%d)/%s", user, pass, port, dbname)
+	db, err = sql.Open("mysql", dataSourceName)
 	if err != nil {
-		fmt.Fprintln(w, "Error Retrieving the File")
-		fmt.Fprintln(w, err)
+		return err
+	}
+	return nil
+}
+
+func createItemHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var item Item
+	err := json.NewDecoder(r.Body).Decode(&item)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	defer file.Close()
 
-	fileBytes, err := io.ReadAll(file)
+	result, err := db.Exec("INSERT INTO items(name) VALUES(?)", item.Name)
 	if err != nil {
-		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	response := sendToOpenAI(fileBytes)
-	w.Write([]byte(response))
+	lastInsertedID, _ := result.LastInsertId()
+	item.ID = int(lastInsertedID)
+
+	jsonItem, _ := json.Marshal(item)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(jsonItem)
 }
 
-func sendToOpenAI(file []byte) string {
-	return "TODO"
+func getAllItemsHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	rows, err := db.Query("SELECT id, name FROM items")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var items []Item
+	for rows.Next() {
+		var item Item
+		err := rows.Scan(&item.ID, &item.Name)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		items = append(items, item)
+	}
+
+	jsonItems, _ := json.Marshal(items)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonItems)
+}
+
+func createSchemaFromDDLFile() error {
+	file, err := os.ReadFile("Sample_table_ddl.sql")
+	if err != nil {
+		return err
+	}
+
+	ddlStatements := strings.Split(string(file), ";")
+	for _, stmt := range ddlStatements {
+		if strings.TrimSpace(stmt) == "" {
+			continue
+		}
+		_, err := db.Exec(stmt)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func updateItemHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Implement update item handler
+}
+
+func deleteItemHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Implement delete item handler
 }
 
 func main() {
+	err := initDB()
+	if err != nil {
+		fmt.Println("Error connecting to database:", err)
+		return
+	}
+
+	err = createSchemaFromDDLFile()
+	if err != nil {
+		fmt.Println("Error creating schema from DDL file:", err)
+		return
+	}
+
+	fmt.Println("Schema created successfully")
+
 	router := httprouter.New()
-	router.GET("/", indexHandler)
-	router.POST("/upload", fileUploadHandler)
+	router.POST("/items", createItemHandler)
+	router.GET("/items", getAllItemsHandler)
+	router.PUT("/items/:id", updateItemHandler)
+	router.DELETE("/items/:id", deleteItemHandler)
+
 	fmt.Println("Running server on :8081")
 	fmt.Println(http.ListenAndServe(":8081", router))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module chat-mannual/cmd
 
 go 1.21.5
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require github.com/go-sql-driver/mysql v1.8.0
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+)
